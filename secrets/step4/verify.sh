5875397aa5b9761655bdc59216ff14daebb55643
amitk-vmware <akumar56@vmware.com>
2024-04-26 17:35:33 +0530

    fix verify sh for step4 secrets

--- secrets/step4/verify.sh
+++ secrets/step4/verify.sh
@@ -9,11 +9,11 @@ EXPECTED_VALUE="v1"
 if kubectl get secret "$SECRET_NAME" -n "$NAMESPACE" &> /dev/null; then
     ACTUAL_VALUE=$(kubectl get secret "$SECRET_NAME" -n "$NAMESPACE" -o jsonpath="{.data.$EXPECTED_KEY}" | base64 -d)
     if [ "$EXPECTED_VALUE" != "$ACTUAL_VALUE" ]; then
-      exist 1
+      exit 1
     fi
 
     if kubectl get pod "$POD_NAME" -n "$NAMESPACE" &> /dev/null; then
-        ACTUAL_SECRET_NAME=$(kubectl get pod app -o jsonpath="{.spec.containers[0].env[?(@.name=='APP_VERSION')].valueFrom.secretKeyRef.name}")
+        ACTUAL_SECRET_NAME=$(kubectl get pod nginx -o jsonpath="{.spec.containers[0].env[?(@.name=='APP_VERSION')].valueFrom.secretKeyRef.name}")
         if [ "$ACTUAL_SECRET_NAME" = "$SECRET_NAME" ]; then
             exit 0
         else
